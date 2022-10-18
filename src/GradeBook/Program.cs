0f177d499c013fe724cdd2cc1045b0cfe584522a
CoolRy <that007guy@hotmail.com>
2022-10-18 19:48:38 -0400

    refactor code

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,42 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var book = new Book("Ryan's Grade Book");
+IBook book = new DiskBook("Ryan's Grade Book");
 book.GradeAdded += OnGradeAdded;
 
+EnterGrades(book);
 
+var stats = book.GetStatistics();
 
-while(true)
-{
-  Console.WriteLine("Enter a grade or 'q' to quit");
-  var input = Console.ReadLine();
-  if(input == "q")
-  {
-    break;
-  }
-  try
-  {
-    var grade = double.Parse(input);
-    book.AddGrade(grade);
-  }
-  catch(ArgumentException ex)
-  {
-    Console.WriteLine(ex.Message);
-  }
-  catch(FormatException ex)
-  {
-    Console.WriteLine(ex.Message);
-  }
-  finally
-  {
-    Console.WriteLine("**");
-  }
-}
-
-
-var stats = book.ShowStatistics();
-
-
-Console.WriteLine($"For the Book Named {book.Name}");
+Console.WriteLine($"For the book named {book.Name}");
 Console.WriteLine($"The Lowest grade is {stats.Low}");
 Console.WriteLine($"The Highest grade is {stats.High}");
 Console.WriteLine($"The Average grade is {stats.Average}");
@@ -45,4 +16,34 @@ Console.WriteLine($"The Letter grade is {stats.Letter}");
 static void OnGradeAdded(object sender, EventArgs e)
 {
   Console.WriteLine("A grade was added");
+}
+
+static void EnterGrades(IBook book)
+{
+  while (true)
+  {
+    Console.WriteLine("Enter a grade or 'q' to quit");
+    var input = Console.ReadLine();
+    if (input == "q")
+    {
+      break;
+    }
+    try
+    {
+      var grade = double.Parse(input);
+      book.AddGrade(grade);
+    }
+    catch (ArgumentException ex)
+    {
+      Console.WriteLine(ex.Message);
+    }
+    catch (FormatException ex)
+    {
+      Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+      Console.WriteLine("**");
+    }
+  }
 }
