0b084ad7dbb95357707e8335bdda250cf0428695
CoolRy <that007guy@hotmail.com>
2022-10-18 11:17:42 -0400

    impliment test

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -7,6 +7,12 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        var x = 5;
+        var y = 2;
+        var expected = 7;
+        var actual = x + y;
+
+        Assert.Equal(expected, actual);
 
     }
 }
