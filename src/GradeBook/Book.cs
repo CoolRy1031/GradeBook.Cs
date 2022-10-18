72315fa716f1147f2da9b25b43edac3a283635d8
CoolRy <that007guy@hotmail.com>
2022-10-18 15:23:54 -0400

    add switch statement

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -6,11 +6,33 @@ class Book
     grades = new List<double>();
     Name = name;
   }
+  public void AddLetterGrade(char letter)
+  {
+    switch(letter)
+    {
+        case 'A':
+          AddGrade(90);
+          break;
+        case 'B':
+          AddGrade(80);
+          break;
+        case 'C':
+          AddGrade(70);
+          break;
+          default:
+          AddGrade(0);
+          break;
+    }
+  }
   public void AddGrade(double grade)
   {
     if (grade <= 100 && grade >= 0)
     {
-    grades.Add(grade);
+      grades.Add(grade);
+    }
+    else 
+    {
+      Console.WriteLine("Invalid Value");
     }
   }
   
@@ -21,6 +43,7 @@ class Book
     result.Average = 0.0;
     result.High = double.MinValue;
     result.Low = double.MaxValue;
+
     foreach(var grade in grades)
     {
     result.Low = Math.Min(grade, result.Low);
@@ -28,6 +51,25 @@ class Book
     result.Average += grade;
     }
     result.Average /= grades.Count;
+
+    switch(result.Average)
+    {
+      case var d when d >= 90.0:
+        result.Letter = 'A';
+        break;
+      case var d when d >= 80.0:
+        result.Letter = 'B';
+        break;
+      case var d when d >= 70.0:
+        result.Letter = 'C';
+        break;
+      case var d when d >= 60.0:
+        result.Letter = 'D';
+        break;
+      default:
+        result.Letter = 'F';
+        break;
+    }
     return result;
   }
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -4,5 +4,11 @@ var book = new Book("Ryan's Grade Book");
 book.AddGrade(89.1);
 book.AddGrade(90.5);
 book.AddGrade(77.5);
-book.ShowStatistics();
+
+var stats = book.ShowStatistics();
+
+Console.WriteLine($"The Lowest grade is {stats.Low}");
+Console.WriteLine($"The Highest grade is {stats.High}");
+Console.WriteLine($"The Average grade is {stats.Average}");
+Console.WriteLine($"The Letter grade is {stats.Letter}");
 
