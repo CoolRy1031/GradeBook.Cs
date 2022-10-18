25728c030f98e247a7789e80d5939dd432c10d27
CoolRy <that007guy@hotmail.com>
2022-10-18 10:48:25 -0400

    add statistics to book

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var book = new Book("Ryan's Grade Book");
+book.AddGrade(89.1);
+book.AddGrade(90.5);
+book.AddGrade(77.5);
+book.ShowStatistics();
+
